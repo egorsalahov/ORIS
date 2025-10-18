8bbe3dd0ef8307d9e3a96bc2ddc4afb4af6187f9
EgorSalahov <egorsalahov2@gmail.com>
2025-10-18 10:00:19 +0300

    httpserver+delta

---
+++ MiniHttpServerEgor/Program.cs
@@ -0,0 +1,43 @@
+﻿using MiniHttpServer.Server;
+using MiniHttpServer.Settings;
+using System.Text;
+
+Singleton singleton = Singleton.GetInstance();
+JsonEntity settings = singleton.Settings;
+
+HttpServer server = new HttpServer(settings);
+
+server.Command = async (context) =>
+{
+
+    //Формируем ответ
+    var response = context.Response;
+    string? responseText = File.ReadAllText("Public/index.html");
+
+
+    //Отправляем html в виде потока байтов
+    byte[] buffer = Encoding.UTF8.GetBytes(responseText);
+
+    response.ContentLength64 = buffer.Length;
+    using Stream output = response.OutputStream;
+
+    await output.WriteAsync(buffer);
+    await output.FlushAsync();
+
+};
+
+
+Task.Run(() =>
+{
+    server.Start();
+});
+
+while (true)
+{
+    if (Console.ReadLine() == "/stop")
+    {
+        server.cts.Cancel();
+        server.Stop();
+        break;
+    }
+}
---
+++ MiniHttpServerEgor/Settings/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer.Settings
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        public JsonEntity Settings { get; private set; }
+
+        private Singleton()
+        {
+            //Берем настройки из settings.json
+            if (!File.Exists("settings.json"))
+            {
+                throw new Exception("Файла settings.json по пути settings.json не существует");
+            }
+
+            string jsonFromFile = File.ReadAllText("settings.json");
+            JsonEntity? jsonEntity = JsonSerializer.Deserialize<JsonEntity>(jsonFromFile);
+
+            if (jsonEntity == null)
+            {
+                throw new Exception("jsonObject пустой!");
+            }
+
+            Settings = jsonEntity;
+        }
+
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
